150b277a0e44d673c2bf9298bc21b48d8449cd7f
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-02-18 15:25:11 -0700

    pausing to create node server for profile form

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
-class Profile extends Component {
+import { reduxForm } from 'redux-form';
 
+class Profile extends Component {
   render() {
+    const { handleSubmit, fields: {
+      firstName, lastName, nickName, email, address, city, state, zip, cellPhone, homePhone, birthday
+      } } = this.props;
+      console.log(firstName);
+
     return(
-      <form className="profile-form">
+      <form className="profile-form" onSubmit={handleSubmit}>
       <h3>Profile Page</h3>
-         <div>
-            <label>Name</label>
+         <div className="form-group">
+            <label>First Name</label><input type="text" className="form-input" {...firstName} />
          </div>
-         <div>
-            <label>Address</label>
+         <div className="form-group">
+            <label>Last Name</label><input type="text" className="form-input" {...lastName} />
          </div>
-         <div>
-            <label>City</label>
+         <div className="form-group">
+            <label>Preferred Name</label><input type="text" className="form-input" {...nickName} />
          </div>
-         <div>
-            <label>State</label>
+         <div className="form-group">
+            <label>Email</label><input type="email" className="form-input" {...email} />
          </div>
-         <div>
-            <label>Zip</label>
+         <div className="form-group">
+            <label>Address</label><input type="text" className="form-input" {...address} />
          </div>
-         <div>
-            <label>Cell Phone</label>
+         <div className="form-group">
+            <label>City</label><input type="text" className="form-input" {...city} />
          </div>
-         <div>
-            <label>Home Phone</label>
+         <div className="form-group">
+            <label>State</label><input type="text" className="form-input" {...state} />
          </div>
-         <div>
-            <label>Birth Date</label>
+         <div className="form-group">
+            <label>Zip</label><input type="number" className="form-input" {...zip} />
          </div>
+         <div className="form-group">
+            <label>Cell Phone</label><input type="tel" className="form-input" {...cellPhone} />
+         </div>
+         <div className="form-group">
+            <label>Home Phone</label><input type="tel" className="form-input" {...homePhone} />
+         </div>
+         <div className="form-group">
+            <label>Birthday</label><input type="date" className="form-input" {...birthday} />
+         </div>
+         <button type="submit" className="nav-btn">Update</button>
       </form>
     );
   }
 }
-export default Profile;
+export default reduxForm({
+   /* config for reduxForm here */
+   form: 'ProfileForm',
+   fields: ['firstName', 'lastName', 'nickName', 'email', 'address', 'city', 'state', 'zip', 'cellPhone', 'homePhone', 'birthday']
+
+})(Profile);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import ProfileReducer from './reducer_profiles';
 import NavLinkReducer from './reducer_nav-links';
 import Campouts from './reducer_campouts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
    profiles: ProfileReducer,
    navLinks: NavLinkReducer,
-   campouts: Campouts
+   campouts: Campouts,
+   form: formReducer
 });
 
 export default rootReducer;
