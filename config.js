52a6ce607ee5660b11b298653b8be425fb23bd3b
khayyamj <khayyam.jones@gmail.com>
2017-04-10 09:34:19 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-export default function () {
-   return {
-    SECRET: '6InAe8q3CAufcmhzTlFr4TfZw5gToDPgLLvlimJ7gW6lK9SRbJBok4b0ra6B2lM8',
-    clientId: 'LKt9y60BcDeJhgdyHJPfL6xYzpMEGsaH',
-    domain: 'khayyam.auth0.com'
-   }
-}
