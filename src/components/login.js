213e9bbd1b4098203f26185745452e51e839796d
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-04-11 17:37:18 -0600

    removed some console.log statements

--- src/components/login.js
+++ src/components/login.js
@@ -23,15 +23,16 @@ export class Login extends React.Component {
       profIndex: null,
       clientid: null
     };
-    localStorage.getItem('id_token') ? token = true : token = false;
+    JSON.parse(localStorage.getItem('profile')) != null ? token = true : token = false;
 
   }
   componentWillMount() {
     this.props.getUserProfiles()
       .then(response => {
         console.log('componentWillMount data: ',response.payload.data);
-        localStorage.getItem('id_token') ? token = true : token = false;
-        token ? this.addToLoginTable() : 'do nothing';
+        console.log( 'componentWillMount--> profile token: ', JSON.parse(localStorage.getItem('profile')) );
+        JSON.parse(localStorage.getItem('profile')) != null ? token = true : token = false;
+        // token ? this.addToLoginTable() : '@componentWillMount--> do nothing';
       });
     console.log('componentWillMount token: ', token);
     if (token) {
@@ -44,23 +45,25 @@ export class Login extends React.Component {
   }
 
   componentWillUpdate(nextProps = []) {
-
-    console.log('componentWillUpdate...', nextProps);
-    if (stopLoop >= 10) {return null}; // end loop, no updates;
-    localStorage.getItem('id_token') ? token = true : token = false;
-    if (!token || nextProps.users.length === 0 || !this.state.clientid) {
-      console.log('ending cycle componentWillUpdate');
-      return null
-    } // no token means nothing to update;
-    stopLoop++;
-    console.log('nextProps.users: ', nextProps.users, 'clientid: ', this.state.clientid);
-    let temp = nextProps.users.find(user => user.clientid == this.state.clientid);
-    console.log('temp index: ', temp);
-    if (this.state.profIndex === temp.loginid) {return null} // already updated - don't update state again;
-    this.setState({ profIndex: temp.loginid})
-    userProfileLoaded === false && temp ? this.updateLoginTable(temp.loginid) : console.log('userProfileLoaded: ', userProfileLoaded, 'temp ', temp);
-    userProfileLoaded === false ? this.addToLoginTable() : console.log('do nothing');
-     console.log(stopLoop,' - componentWillUpdate interation');
+    setTimeout(() => {
+      console.log('componentWillUpdate...', nextProps);
+      // if (stopLoop >= 10) {return null}; // end loop, no updates;
+      console.log('componentWillUpdate-->', !token, nextProps.users.length === 0, !this.state.clientid)
+      JSON.parse(localStorage.getItem('profile')) != null ? token = true : token = false;
+      if (!token || nextProps.users.length === 0 || !this.state.clientid) {
+        console.log('<- ending cycle componentWillUpdate ->');
+        return null
+      } // no token means nothing to update;
+        stopLoop++;
+        console.log('nextProps.users: ', nextProps.users, 'clientid: ', this.state.clientid);
+        let temp = nextProps.users.find(user => user.clientid == this.state.clientid);
+        console.log('temp index: ', temp, this.state.profIndex, temp.loginid);
+        if (this.state.profIndex === temp.loginid) {return null} // already updated - don't update state again;
+          this.setState({ profIndex: temp.loginid})
+          userProfileLoaded === false && temp ? this.updateLoginTable(temp.loginid) : console.log('userProfileLoaded: ', userProfileLoaded, 'temp ', temp);
+          userProfileLoaded === false ? this.addToLoginTable() : console.log("don't add to login table");
+          console.log(stopLoop,' - componentWillUpdate interation');
+    }, 1000);
   }
 
   logout() {
@@ -73,27 +76,28 @@ export class Login extends React.Component {
   render() {
     const { auth } = this.props;
     token === this.state.login ? console.log('token match: ', token) : this.loginStatus();
+    console.log('(render) login: ', this.props.login.login);
     return (
       <div style={{textAlign: 'center'}}>
-        <Header centered>
-          {this.props.login.login ? "Thank you for visiting our page. Please remember to logout when you're done." : "Please log in to view additional troop information"}
-        </Header>
-          <Button
-            animated='fade'
-            color='green'
-            onClick={auth.login.bind(this)}
-            style={{ display: this.props.login.login ? "none": "inline-block"}}>
-            <Button.Content visible>Login </Button.Content>
-            <Button.Content hidden><Icon name='sign in' /></Button.Content>
-          </Button>
-          <Button
-            animated='fade'
-            color='red'
-            onClick={this.logout.bind(this)}
-            style={{ display: this.props.login.login ? "inline-block" : "none"}}>
-            <Button.Content visible>Logout </Button.Content>
-            <Button.Content hidden><Icon name='sign out' /></Button.Content>
-          </Button>
+      <Header centered>
+      {this.props.login.login ? "Thank you for visiting our page. Please remember to logout when you're done." : "Please log in to view additional troop information"}
+      </Header>
+      <Button
+      animated='fade'
+      color='green'
+      onClick={auth.login.bind(this)}
+      style={{ display: this.props.login.login ? "none": "inline-block"}}>
+      <Button.Content visible>Login </Button.Content>
+      <Button.Content hidden><Icon name='sign in' /></Button.Content>
+      </Button>
+      <Button
+      animated='fade'
+      color='red'
+      onClick={this.logout.bind(this)}
+      style={{ display: this.props.login.login ? "inline-block" : "none"}}>
+      <Button.Content visible>Logout </Button.Content>
+      <Button.Content hidden><Icon name='sign out' /></Button.Content>
+      </Button>
       </div>
     )
   }
@@ -101,7 +105,7 @@ export class Login extends React.Component {
   loginStatus() {
     console.log('login--> loginStatus function');
     this.setState({ login: !login });
-    localStorage.getItem('id_token') ? this.props.loggedIn() : this.props.loggedOut();
+    JSON.parse(localStorage.getItem('profile')) != null ? this.props.loggedIn() : this.props.loggedOut();
     this.setState({ clientid: JSON.parse(localStorage.getItem('profile')).clientID })
   }
 
@@ -121,9 +125,10 @@ export class Login extends React.Component {
     };
     console.log('loginProfileObj: ', loginProfileObj);
     this.props.createLoginProfile(loginProfileObj)
-      userProfileLoaded = true;
-      console.log('userProfileLoaded status updated')
+    userProfileLoaded = true;
+    console.log('userProfileLoaded status updated')
   }
+
   updateLoginTable(id){
     if (userProfileLoaded) {return 'completed'}
     console.log('updateLoginTable...', id);
