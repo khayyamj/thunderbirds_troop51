7a9caf2f894a0f19742cbb5ff953b39052e62472
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-04-12 14:57:12 -0600

    upgrading webpack to build into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: 'dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
-  devtool: [
-     '#source-map'
- ],
+  devtool: "source-map",
   module: {
-    preloaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: 'node_modules',
-        loader: 'jshint-loader'
-      }
-    ],
-   loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-   },
-   {
-     test: /\.css$/,
-     exclude: /node_modules/,
-     loader: "style-loader!css-loader"
-   },
-   {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      loader: "style-loader!css-loader!sass-loader"
-   },
+        enforce: "pre",
+        loader: "jshint-loader"
+      },
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+     },
+     {
+       test: /\.css$/,
+       exclude: /node_modules/,
+       loader: "style-loader!css-loader"
+     },
+     {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: "style-loader!css-loader!sass-loader"
+     },
    ]
   },
   plugins: [
@@ -47,10 +46,23 @@ module.exports = {
            host: 'localhost',
            port: 3000,
            proxy: 'http://localhost:8080/'
+        }),
+        new HtmlWebpackPlugin({
+          title: 'Troop 51 Thunderbirds',
+          hash: true,
+          template: 'index.html', // Load a custom template
+        }),
+        new UglifyJSPlugin({
+          sourceMap: true
+        }),
+        new ExtractTextPlugin({
+          filename: "bundle.css",
+          disable: false,
+          allChunks: true
         })
    ],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
